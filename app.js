94d89f34f5358b75d3058fba50a8626f06fed1dd
MAC <mac@MACs-MacBook-Pro.local>
2026-06-25 20:10:57 +0100

    Updated CORS3

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const paymentRoutes = require("./src/routes/fees/paymentRoutes");
 const discountRoutes = require("./src/routes/fees/discountRoutes");
 const feeReportRoutes = require("./src/routes/fees/feeReportRoutes");
 
-
+console.log("Allowed origins:", allowedOrigins);
 
 // const notFound = require("./src/middleware/notFound");
 const {notFound, errorHandler} = require("./src/middleware/errorHandler");
@@ -45,29 +45,29 @@ const allowedOrigins = process.env.FRONTEND_URL
 app.use(helmet());
 app.use(compression());
 
-// app.use(
-//   cors({
-//     origin(origin, callback) {
-//       if (!origin || allowedOrigins.includes(origin)) {
-//         return callback(null, true);
-//       } 
-      
-//       return callback(new Error("Not allowed by CORS"));
-//     },
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//   })
-// );
-
 app.use(
   cors({
-    origin: "moonlightcollege.com.ng",
+    origin(origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } 
+      
+      return callback(new Error("Not allowed by CORS"));
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+// app.use(
+//   cors({
+//     origin: "moonlightcollege.com.ng",
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
 
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
